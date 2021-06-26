453a6513404a0940ce3bfaac99d2189059a6216f
jasminearabgard <yasaman.arabgard@outlook.com>
2021-06-26 23:43:38 +0430

    fix bugs and finish

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ let submitBtn=document.getElementById("submitButton");
 let usererror=document.getElementById("usernameerror");
 let phoneerror=document.getElementById("phoneerror");
 let passworderror=document.getElementById("passworderror");
+let randompass=document.getElementById("pass-btn");
 const pageLoad = () => submitBtn.onclick = Checkcycle();
 
 
@@ -14,7 +15,6 @@ const Checkcycle=()=>{
    let passInput= password.value;
    let userreg = /^[A-Za-z]+$/;
    let phonereg=/^(\+98?)?{?(0?9[0-9]{9,9}}?)$/;
-   let passreg=/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]$/;
 
    if(userInput == ""){
        usererror.innerHTML="type something please!";
@@ -27,19 +27,40 @@ const Checkcycle=()=>{
    if(phoneInput == ""){
        phoneerror.innerHTML="type something please!";
    }else if(!phoneInput.match(phonereg)){
-       phoneerror.innerHTML="phonenumber must be 0-9 & started with +98";
+       phoneerror.innerHTML="phonenumber must be 0-9";
    }else{
        phoneerror.innerHTML= " ";
    }
 
    if(passInput == ""){
      passworderror.innerHTML="type something please!";
-   }else if(!passInput.match(passreg)){
-       passworderror.innerHTML="The password must have at least one special character";
+     randompass.style.display="none";
+   }else if(passInput.length < 8){
+       passworderror.innerHTML="The password must be more 8 character";
+       randompass.style.display="none";
    }else{
        passworderror.innerHTML="";
+       randompass.style.display="none";
    }
 }
 
-
 submitBtn.addEventListener("click",Checkcycle);
+
+randompass.addEventListener("click",()=>{
+     let chars="0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz-|@.,?/!~#$%^&*(){}[]+=";
+     let passlength = 8;
+     let randompass ="";
+     for(let i=0 ; i < passlength ; i++){
+         let random = Math.floor(Math.random() * chars.length);
+         randompass += chars.substring(random,random + 1 );
+     }
+     password.value = randompass;
+     hideBtn();
+})
+
+password.addEventListener("click",()=>{
+    randompass.style.display="block";
+})
+
+
+const hideBtn=()=>randompass.style.display="none";
